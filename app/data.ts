f043238d3d57ec3233ae0dc5c9008d3bf8d61749
Kelvym Miranda <kelvymm@gmail.com>
2024-03-01 01:16:26 -0300

    feat: Add mock API request

---
+++ app/data.ts
@@ -0,0 +1,90 @@
+export const dataTasks = [
+  {
+    id: 1,
+    type: 'task',
+    title: 'HTMX',
+    description: '',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+    level: 1,
+    isFinished: false,
+  },
+] as const
+
+export const dataNotes = [
+  {
+    id: 1,
+    title: 'how do create something',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sodales massa. Sed efficitur, ex vel tempus ultricies, purus enim finibus ligula, ac congue dolor massa in nibh. Donec tempor iaculis leo, vitae dapibus tellus tempus ac. Duis sit amet dui a tellus convallis suscipit vitae sit amet risus. Nam laoreet purus diam, porttitor dignissim ante porta at. Mauris at auctor nibh. Morbi aliquam elementum orci, eget fringilla arcu ornare et. Aenean convallis orci nec nulla consequat, at consequat ligula blandit. Sed sit amet egestas leo. Aliquam condimentum consectetur posuere. Cras nec nisl eu odio lacinia sodales. Phasellus quam massa, rhoncus eu cursus in, venenatis sit amet felis. Nulla ac pulvinar felis. In lacinia quis lectus ac pretium. Vivamus congue nibh molestie est semper sagittis.
+
+      Vestibulum dignissim leo orci. Suspendisse molestie nulla vitae semper vulputate. Vivamus non finibus neque, quis tempus neque. Aliquam condimentum, odio in varius rutrum, arcu ligula dictum turpis, non malesuada magna nunc nec arcu. Suspendisse potenti. Pellentesque sagittis et est quis tempus. Aenean mattis vitae elit nec pretium. Nullam arcu metus, posuere sed augue in, placerat maximus odio. Sed et bibendum dui, ac suscipit velit. Donec dignissim urna turpis, at fermentum libero ornare malesuada. Morbi id est mattis, accumsan sapien id, elementum ante. Aliquam in urna ac magna cursus cursus quis ut libero. Aliquam pulvinar ut nibh nec auctor. Nam maximus nisi vestibulum, mattis lectus sed, mollis est.
+      
+      Vestibulum in purus volutpat, maximus elit mattis, cursus lacus. Nunc eget risus fermentum metus consequat aliquet ut ac arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque fermentum ex eget sagittis porta. In hac habitasse platea dictumst. Integer a turpis nec ipsum ultrices viverra nec ac purus. Maecenas arcu sapien, accumsan nec porttitor et, sollicitudin quis risus. Donec in sapien tempor ante porta congue. Vivamus non felis et dolor eleifend suscipit ac vel erat. Suspendisse ligula risus, lobortis ut maximus eu, mollis nec erat. Aenean hendrerit urna sit amet tristique tristique. Duis vitae molestie massa. Nullam varius leo sit amet velit malesuada faucibus. Sed in mi in dui tempor posuere. Donec in volutpat mi.`,
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 2,
+    title: 'config to X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 3,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 4,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 5,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 6,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 7,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 8,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+  {
+    id: 9,
+    title: 'how to use X',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
+    dateCreated: '2017-01-01',
+    dateFinished: '',
+  },
+] as const
