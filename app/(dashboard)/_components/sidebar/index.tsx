447353525ce07b27c4e774fd493be140ff1fa431
Kelvym Miranda <kelvymm@gmail.com>
2024-03-01 23:40:11 -0300

    fix: Main style structure

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -3,8 +3,8 @@ import { Navigation } from './navigation'
 
 export const Sidebar = () => {
   return (
-    <nav className="w-20 md:w-60 h-full bg-black p-5">
-      <Navigation></Navigation>
+    <nav className="basis-10 md:basis-60 h-full bg-dark p-5 shrink-0">
+      <Navigation />
       <UserButton />
     </nav>
   )
