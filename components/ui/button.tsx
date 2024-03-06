8fe90c510fe6fc4d8389e2d1b44371935ba785da
Kelvym Miranda <kelvymm@gmail.com>
2024-03-06 00:32:22 -0300

    feat: Add handle to icon and loading to the Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
+import { LucideIcon } from 'lucide-react'
 
 const buttonVariants = cva(
   'gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -32,22 +33,76 @@ const buttonVariants = cva(
     },
   }
 )
-
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  loading?: boolean
+  icon?: LucideIcon
+}
+
+const sizes = {
+  default: 14,
+  sm: 16,
+  lg: 20,
+  icon: 16,
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      loading,
+      size,
+      asChild = false,
+      disabled,
+      icon: Icon,
+      children,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        disabled={loading ? true : disabled}
         ref={ref}
         {...props}
-      />
+      >
+        {loading && (
+          <div
+            className="py-3"
+            style={{ width: sizes[size as keyof typeof sizes] }}
+          >
+            <svg
+              className="animate-spin h-full text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
+        {Icon && loading === false ? (
+          <Icon size={sizes[size as keyof typeof sizes] || sizes.default} />
+        ) : null}
+        {children}
+      </Comp>
     )
   }
 )
