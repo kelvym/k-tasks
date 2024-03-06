2c4983c46f3916aebe689432a1209f3f9eafa57e
Kelvym Miranda <kelvymm@gmail.com>
2024-03-06 01:10:06 -0300

    feat: Add badge component

---
+++ components/ui/badge.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
+
+const badgeVariants = cva(
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent bg-background-secondary text-foreground hover:bg-primary/80',
+        secondary:
+          'border-transparent bg-primary text-secondary-foreground hover:bg-secondary/80',
+        destructive:
+          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        outline: 'text-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {
+  loading?: boolean
+}
+
+function Badge({
+  className,
+  variant,
+  loading,
+  children,
+  ...props
+}: BadgeProps) {
+  return (
+    <div
+      className={cn('flex gap-1', badgeVariants({ variant }), className)}
+      {...props}
+    >
+      {loading && (
+        <svg
+          className="animate-spin text-white w-2"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      )}
+      {children}
+    </div>
+  )
+}
+
+export { Badge, badgeVariants }
+
+{
+  /* <div style="
+    display: flex;
+    align-items: end;
+    gap: 15px;
+"><h1 class="text-2xl text-header" style="
+    line-height: 100%;
+">Edit</h1><div style="
+    border: 1px solid #515151;
+    border-radius: 11px;
+    font-size: 11px;
+    padding: 2px 11px;
+    background: #282828;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 7px;
+"><svg class="animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="
+    width: 10px;
+">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg><div>Saving...</div></div></div> */
+}
